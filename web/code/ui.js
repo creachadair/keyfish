1bff43223e2635f547f5d750ad700debabb417be
M. J. Fromberger <michael.j.fromberger@gmail.com>
2014-07-06 23:38:55 -0700

    Update the site when populating from the URL bar.

--- web/code/ui.js
+++ web/code/ui.js
@@ -49,7 +49,9 @@ document.addEventListener("DOMContentLoaded", function() {
     if (typeof chrome != 'undefined') {
 	document.body.style.minWidth = "380px";
 	getBrowserHost(function (host) {
-	    document.forms.kf.siteName.value = trimHost(host || getPreviousHost());
+	    document.forms.kf.siteName.value =
+		trimHost(host || getPreviousHost());
+	    uiSiteChanged();
 	});
     }
 
