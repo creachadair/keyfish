3d6b72df80c01bede4085f8ab989d9b129d2b6c4
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-09-07 16:46:35 -0700

    Simplify interaction with the clipboard tools.

--- clip_darwin.go
+++ clip_darwin.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"io"
-	"log"
 	"os/exec"
+	"strings"
 )
 
 // toClipboard attempts to copy the given password to the system clipboard.
 func toClipboard(pw string) error {
 	cmd := exec.Command("pbcopy")
-	p, err := cmd.StdinPipe()
-	if err != nil {
-		return err
-	}
-	if err = cmd.Start(); err != nil {
-		return err
-	}
-	io.WriteString(p, pw)
-
-	// We must close the pipe, so the process can exit.
-	if err := p.Close(); err != nil {
-		log.Printf("Error closing pipe: %v", err)
-	}
-	return cmd.Wait()
+	cmd.Stdin = strings.NewReader(pw)
+	return cmd.Run()
 }
--- clip_linux.go
+++ clip_linux.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"errors"
-	"fmt"
-	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // toClipboard attempts to copy the given password to the system clipboard.
@@ -15,18 +14,6 @@ func toClipboard(pw string) error {
 		return errors.New("unable to copy to clipboard (no DISPLAY)")
 	}
 	cmd := exec.Command("xsel", "--clipboard")
-	p, err := cmd.StdinPipe()
-	if err != nil {
-		return err
-	}
-	if err = cmd.Start(); err != nil {
-		return err
-	}
-	fmt.Fprint(p, pw)
-
-	// We must close the pipe, so the process can exit.
-	if err := p.Close(); err != nil {
-		log.Printf("Error closing pipe: %v", err)
-	}
-	return cmd.Wait()
+	cmd.Stdin = strings.NewReader(pw)
+	return cmd.Run()
 }
