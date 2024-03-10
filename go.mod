371c2ce377d9c1c980c17a945fc916196fe464a9
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-03-09 19:05:47 -0800

    keyfish: refactor main program to use the command library
    No functional changes are intended. This is just moving the code around to take
    advantage of the command plumbing for help and flags.

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/creachadair/keyfish
 require (
 	bitbucket.org/creachadair/shell v0.0.8
 	bitbucket.org/creachadair/stringset v0.0.14
+	github.com/creachadair/command v0.1.10
 	github.com/creachadair/getpass v0.2.1
+	github.com/creachadair/mds v0.12.0
 	github.com/creachadair/otp v0.4.2
 	github.com/google/go-cmp v0.6.0
 	github.com/tdewolff/minify/v2 v2.20.18
--- keyfish.go
+++ keyfish.go
@@ -2,7 +2,8 @@
 // algorithm.
 //
 // Basic usage:
-//    keyfish some.site.com
+//
+//	keyfish some.site.com
 //
 // The tool will prompt at the terminal for a password, and will print the
 // resulting password to stdout.  Use the -copy option to instead copy the
@@ -21,14 +22,13 @@
 // KEYFISH_CONFIG environment variable to the path of the configuration file
 // and run "go generate ./config" before building:
 //
-//   git clone https://github.com/creachadair/keyfish
-//   cd keyfish
-//   env KEYFISH_CONFIG=$HOME/my-config.json go generate ./config
-//   go build
+//	git clone https://github.com/creachadair/keyfish
+//	cd keyfish
+//	env KEYFISH_CONFIG=$HOME/my-config.json go generate ./config
+//	go build
 //
 // You can then copy the keyfish binary where you like and it will use the
 // static configuration unless you override it at runtime with KEYFISH_CONFIG.
-
 package main
 
 import (
@@ -45,66 +45,56 @@ import (
 
 	"bitbucket.org/creachadair/shell"
 	"bitbucket.org/creachadair/stringset"
+	"github.com/creachadair/command"
 	"github.com/creachadair/getpass"
 	"github.com/creachadair/keyfish/clipboard"
 	"github.com/creachadair/keyfish/config"
 	"github.com/creachadair/keyfish/wordhash"
+	"github.com/creachadair/mds/value"
 	"github.com/creachadair/otp"
 )
 
 const minLength = 6 // Allow no passwords shorter than this
 
 var (
-	cfg *config.Config // see init below
+	// Set up a default configuration to use if one is not loaded from a file.
+	cfg = &config.Config{
+		Default: config.Site{Length: 18, Punct: value.Ptr(true)},
+	}
 
-	secretKey = os.Getenv("KEYFISH_SECRET")
-	doSites   bool // list known site configuations
-	doShow    bool // show the named configurations
-	doPrint   bool // print the result, overriding -copy
-	doPunct   bool // enable punctuation, overriding the default
+	doSites bool // list known site configuations
+	doShow  bool // show the named configurations
+	doPrint bool // print the result, overriding -copy
+	doPunct bool // enable punctuation, overriding the default
 )
 
-func init() {
-	// Set up a default configuration to use if one is not loaded from a file.
-	usePunct := true
-	cfg = &config.Config{
-		Default: config.Site{
-			Length: 18, Punct: &usePunct,
-		},
+func main() {
+	cfPath := config.FilePath() // for cosmetics in Help, see below
+	if tail, ok := strings.CutPrefix(cfPath, os.Getenv("HOME")); ok {
+		cfPath = "~" + tail
 	}
-}
 
-func init() {
-	flag.IntVar(&cfg.Default.Length, "length", 18, "Password length")
+	// Load configuration settings from the user's file, if it exists.
+	// Do this prior to flag parsing so that flags can override defaults.
+	if err := cfg.Load(config.FilePath()); err != nil && !os.IsNotExist(err) {
+		log.Fatalf("Loading configuration: %v", err)
+	}
 
-	flag.StringVar(&cfg.Default.Format, "format", "", "Password format")
-	flag.StringVar(&cfg.Default.Salt, "salt", "", "Salt to hash with the site name")
-	flag.BoolVar(&doSites, "list", false, "List known sites and exit")
-	flag.BoolVar(&doShow, "show", false, "Print specified configurations and exit")
-	flag.BoolVar(&doPrint, "print", false, "Print the result rather than copying (overrides -copy)")
-	flag.BoolVar(&doPunct, "punct", false, "Use punctuation, overriding the default")
-	flag.BoolVar(&cfg.Flags.Verbose, "v", false, "Verbose logging (includes hints with -print)")
-	flag.BoolVar(&cfg.Flags.Copy, "copy", false, "Copy to clipboard instead of printing")
-	flag.BoolVar(&cfg.Flags.OTP, "otp", false, "Generate an OTP for the site (if configured)")
-	flag.BoolVar(&cfg.Flags.Strict, "strict", false, "Report an error for sites not named in the config")
+	root := &command.C{
+		Name:  filepath.Base(os.Args[0]),
+		Usage: `[options] [tag@]site-name ...`,
+		Help: `Generate a site-specific password based on the given site name.
 
-	flag.Usage = func() {
-		cf := config.FilePath()
-		if t := strings.TrimPrefix(cf, os.Getenv("HOME")); t != cf {
-			cf = "~" + t
-		}
-		fmt.Fprintf(os.Stderr, `Usage: %[1]s [options] <site.name>+
+The resulting password is printed to stdout, or copied to the clipboard if --copy is set.
 
-Generates a site-specific password based on the given site name.  The resulting
-password is printed to stdout, or copied to the clipboard if --copy is set.
+If the KEYFISH_SECRET environment variable is set, it is used as the passphrase for
+password generation.  Otherwise, the user is prompted at the terminal.
 
-If the KEYFISH_SECRET environment variable is set, it is used as the passphrase
-for password generation.  Otherwise, the user is prompted at the terminal.
-However, if KEYFISH_SECRET ends with a "|" symbol, it is instead treated as a
-command line to execute to return the passphrase.
+However, if KEYFISH_SECRET ends with a "|" symbol, it is instead treated as a command
+line to execute to return the passphrase.
 
-Use --format to specify an exact password layout, with wildcards to substitute
-elements of the alphabet:
+Use --format to specify an exact password layout, with wildcards to substitute elements
+of the alphabet:
 
   ^   for an uppercase letter
   _   for a lowercase letter
@@ -115,19 +105,131 @@ elements of the alphabet:
 
 All other characters are copied verbatim.
 
-A site name has the form "host.org" or "salt@host.org". If the site matches one
-of the sites named in the user's config file, the corresponding settings are used.
+A site name has the form "host.org" or "salt@host.org". If the site matches one of the
+sites named in the user's config file, the corresponding settings are used.
 
-By default, configuration is read from %[2]s.
+By default, configuration is read from ` + cfPath + `
 If KEYFISH_CONFIG is set, that path is used instead.
+`,
 
-Flags:`+"\n", filepath.Base(os.Args[0]), cf)
-		flag.PrintDefaults()
+		SetFlags: func(env *command.Env, fs *flag.FlagSet) {
+			fs.IntVar(&cfg.Default.Length, "length", 18, "Password length")
+			fs.StringVar(&cfg.Default.Format, "format", "", "Password format")
+			fs.StringVar(&cfg.Default.Salt, "salt", "", "Salt to hash with the site name")
+			fs.BoolVar(&doSites, "list", false, "List known sites and exit")
+			fs.BoolVar(&doShow, "show", false, "Print specified configurations and exit")
+			fs.BoolVar(&doPrint, "print", false, "Print the result rather than copying (overrides -copy)")
+			fs.BoolVar(&doPunct, "punct", false, "Use punctuation, overriding the default")
+			fs.BoolVar(&cfg.Flags.Verbose, "v", false, "Verbose logging (includes hints with -print)")
+			fs.BoolVar(&cfg.Flags.Copy, "copy", false, "Copy to clipboard instead of printing")
+			fs.BoolVar(&cfg.Flags.OTP, "otp", false, "Generate an OTP for the site (if configured)")
+			fs.BoolVar(&cfg.Flags.Strict, "strict", false, "Report an error for sites not named in the config")
+		},
+
+		Run: func(env *command.Env) error {
+			// Unless we're listing sites, at least one must be requested.
+			if doSites {
+				listSites(os.Stdout, stringset.FromKeys(cfg.Sites))
+				return nil
+			} else if len(env.Args) == 0 {
+				return env.Usagef("You must specify at least one site name")
+			}
+			if doShow {
+				out := json.NewEncoder(os.Stdout)
+				if cfg.Flags.Verbose {
+					out.SetIndent("", "  ")
+				}
+				for _, arg := range env.Args {
+					var site config.Site
+					var ok bool
+
+					for _, c := range config.SiteCandidates(arg) {
+						site, ok = cfg.Site(c)
+						if ok {
+							break
+						}
+					}
+					if !ok && cfg.Flags.Strict {
+						return fmt.Errorf("site %q is not known", arg)
+					}
+					if !cfg.Flags.Verbose {
+						site.Hints = nil
+						site.OTP = nil
+					}
+					if err := out.Encode(site); err != nil {
+						return fmt.Errorf("encoding site %q: %v", arg, err)
+					}
+				}
+				return nil
+			}
+
+			// Check all the sites before generating any secrets.
+			var sites []config.Site
+			for _, arg := range env.Args {
+				var site config.Site
+				var ok bool
+
+				for _, c := range config.SiteCandidates(arg) {
+					site, ok = cfg.Site(c)
+					if ok {
+						break
+					}
+				}
+				if !ok && cfg.Flags.Strict {
+					return fmt.Errorf("site %q is not known", arg)
+				}
+				sites = append(sites, site)
+			}
+
+			for _, site := range sites {
+				// Check minimum length.
+				if site.Length < minLength {
+					return fmt.Errorf("password length must be ≥ %d", minLength)
+				} else if site.Format != "" && len(site.Format) < minLength {
+					return fmt.Errorf("format length must be ≥ %d", minLength)
+				}
+
+				if isFlagSet(&env.Command.Flags, "punct") {
+					site.Punct = &doPunct // override whatever was there
+				}
+				if cfg.Flags.Verbose {
+					log.Printf("Site: %v", site)
+				}
+
+				key, err := loadKeyIfNeeded()
+				if err != nil {
+					return fmt.Errorf("loading secret key: %w", err)
+				}
+				ctx := site.Context(key)
+				var pw string
+				if fmt := site.Format; fmt != "" {
+					pw = ctx.Format(fmt)
+				} else {
+					pw = ctx.Password(site.Length)
+				}
+				if doPrint || !cfg.Flags.Copy {
+					fmt.Println(pw)
+				} else if err := clipboard.WriteString(pw); err != nil {
+					log.Printf("Error copying to clipboard: %v", err)
+				} else {
+					if u := site.Login; u != "" {
+						fmt.Print(u, "@")
+					}
+					fmt.Print(site.Host, "\t", wordhash.String(pw))
+					if cfg.Flags.OTP {
+						otpc, ok := site.OTP[site.Salt]
+						if ok {
+							fmt.Print("\t", otp.Config{Key: string(otpc.Key)}.TOTP())
+						}
+					}
+					fmt.Println()
+				}
+			}
+			return nil
+		},
 	}
+	command.RunOrFail(root.NewEnv(nil), os.Args[1:])
 }
-
-// usage prints a usage message to stderr and terminates the program.
-func fail(msg string, args ...interface{}) { log.Fatalf(msg, args...) }
 
 // listSites renders a nicely-formatted listing of sites to w.
 func listSites(w io.Writer, sites stringset.Set) {
@@ -174,129 +276,6 @@ func listSites(w io.Writer, sites stringset.Set) {
 	tw.Flush()
 }
 
-func main() {
-	// Load configuration settings from the user's file, if it exists.
-	// Do this prior to flag parsing so that flags can override defaults.
-	if err := cfg.Load(config.FilePath()); err != nil && !os.IsNotExist(err) {
-		fail("Error loading configuration: %v", err)
-	}
-
-	flag.Parse()
-
-	// Unless we're listing sites, at least one must be requested.
-	if doSites {
-		listSites(os.Stdout, stringset.FromKeys(cfg.Sites))
-		return
-	} else if flag.NArg() == 0 {
-		fail("You must specify at least one site name")
-	}
-	if doShow {
-		out := json.NewEncoder(os.Stdout)
-		if cfg.Flags.Verbose {
-			out.SetIndent("", "  ")
-		}
-		for _, arg := range flag.Args() {
-			var site config.Site
-			var ok bool
-
-			for _, c := range config.SiteCandidates(arg) {
-				site, ok = cfg.Site(c)
-				if ok {
-					break
-				}
-			}
-			if !ok && cfg.Flags.Strict {
-				fail("Site %q is not known", arg)
-			}
-			if !cfg.Flags.Verbose {
-				site.Hints = nil
-				site.OTP = nil
-			}
-			if err := out.Encode(site); err != nil {
-				fail("Error encoding site %q: %v", arg, err)
-			}
-		}
-		return
-	}
-
-	// Establish the secret key, on demand.
-	mustLoadKey := func() string {
-		if secretKey == "" {
-			pw, err := getpass.Prompt("Secret key: ")
-			if err != nil {
-				fail("Error reading secret key: %v", err)
-			}
-			secretKey = pw
-		} else if pc, ok := isPipeCommand(secretKey); ok {
-			pw, err := exec.Command(pc[0], pc[1:]...).Output()
-			if err != nil {
-				fail("Error reading secret key: %v", err)
-			}
-			secretKey = strings.TrimSuffix(string(pw), "\n")
-		}
-		return secretKey
-	}
-
-	// Check all the sites before generating any secrets.
-	var sites []config.Site
-	for _, arg := range flag.Args() {
-		var site config.Site
-		var ok bool
-
-		for _, c := range config.SiteCandidates(arg) {
-			site, ok = cfg.Site(c)
-			if ok {
-				break
-			}
-		}
-		if !ok && cfg.Flags.Strict {
-			fail("Site %q is not known", arg)
-		}
-		sites = append(sites, site)
-	}
-
-	for _, site := range sites {
-		// Check minimum length.
-		if site.Length < minLength {
-			fail("Password length must be ≥ %d", minLength)
-		} else if site.Format != "" && len(site.Format) < minLength {
-			fail("Format length must be ≥ %d", minLength)
-		}
-
-		if isFlagSet("punct") {
-			site.Punct = &doPunct // override whatever was there
-		}
-		if cfg.Flags.Verbose {
-			log.Printf("Site: %v", site)
-		}
-
-		ctx := site.Context(mustLoadKey())
-		var pw string
-		if fmt := site.Format; fmt != "" {
-			pw = ctx.Format(fmt)
-		} else {
-			pw = ctx.Password(site.Length)
-		}
-		if doPrint || !cfg.Flags.Copy {
-			fmt.Println(pw)
-		} else if err := clipboard.WriteString(pw); err != nil {
-			log.Printf("Error copying to clipboard: %v", err)
-		} else {
-			if u := site.Login; u != "" {
-				fmt.Print(u, "@")
-			}
-			fmt.Print(site.Host, "\t", wordhash.String(pw))
-			if cfg.Flags.OTP {
-				otpc, ok := site.OTP[site.Salt]
-				if ok {
-					fmt.Print("\t", otp.Config{Key: string(otpc.Key)}.TOTP())
-				}
-			}
-			fmt.Println()
-		}
-	}
-}
-
 func isPipeCommand(key string) ([]string, bool) {
 	if t := strings.TrimSuffix(key, "|"); t != key {
 		return shell.Split(t)
@@ -304,12 +283,32 @@ func isPipeCommand(key string) ([]string, bool) {
 	return nil, false
 }
 
-func isFlagSet(name string) bool {
+func isFlagSet(fs *flag.FlagSet, name string) bool {
 	var ok bool
-	flag.Visit(func(f *flag.Flag) {
+	fs.Visit(func(f *flag.Flag) {
 		if f.Name == name {
 			ok = true
 		}
 	})
 	return ok
+}
+
+// loadKeyIfNeeded prompts the user for a secret key if needed, handling the
+// case where the prompt command requires shelling out.
+func loadKeyIfNeeded() (string, error) {
+	secretKey := os.Getenv("KEYFISH_SECRET")
+	if secretKey == "" {
+		pw, err := getpass.Prompt("Secret key: ")
+		if err != nil {
+			return "", fmt.Errorf("reading secret key: %v", err)
+		}
+		secretKey = pw
+	} else if pc, ok := isPipeCommand(secretKey); ok {
+		pw, err := exec.Command(pc[0], pc[1:]...).Output()
+		if err != nil {
+			return "", fmt.Errorf("reading secret key: %v", err)
+		}
+		secretKey = strings.TrimSuffix(string(pw), "\n")
+	}
+	return secretKey, nil
 }
