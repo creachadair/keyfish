20effdb2fa3e66e64ae304905ac13e116c8a845b
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-04-21 15:06:21 -0700

    cmd/kf: add "debug hashpass" subcommand

--- cmd/kf/internal/cmddebug/cmddebug.go
+++ cmd/kf/internal/cmddebug/cmddebug.go
@@ -4,12 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/creachadair/command"
+	"github.com/creachadair/flax"
 	"github.com/creachadair/keyfish/cmd/kf/config"
 	"github.com/creachadair/keyfish/cmd/kf/internal/cmdconvert"
 	"github.com/creachadair/keyfish/kfdb"
 	"github.com/creachadair/keyfish/kflib"
+	"github.com/creachadair/mds/value"
 )
 
 var Command = &command.C{
@@ -21,17 +24,26 @@ Use "@" to refer to the path set via the --db flag.`,
 
 	Unlisted: true,
 
-	Commands: []*command.C{{
-		Name:  "export",
-		Usage: "<db-path>",
-		Help:  "Export the contents of a database in plaintext as JSON.",
-		Run:   command.Adapt(runDebugExport),
-	}, {
-		Name:  "import",
-		Usage: "<db-path> <json-path>",
-		Help:  "Import a plaintext JSON into a database, replacing its contents.",
-		Run:   command.Adapt(runDebugImport),
-	},
+	Commands: []*command.C{
+		{
+			Name:  "export",
+			Usage: "<db-path>",
+			Help:  "Export the contents of a database in plaintext as JSON.",
+			Run:   command.Adapt(runDebugExport),
+		},
+		{
+			Name:  "import",
+			Usage: "<db-path> <json-path>",
+			Help:  "Import a plaintext JSON into a database, replacing its contents.",
+			Run:   command.Adapt(runDebugImport),
+		},
+		{
+			Name:     "hashpass",
+			Usage:    "[flags] [salt]@seed",
+			Help:     "Generate an HKDF based hash password or passphrase.",
+			SetFlags: command.Flags(flax.MustBind, &hpFlags),
+			Run:      command.Adapt(runDebugHashpass),
+		},
 		cmdconvert.Command,
 	},
 }
@@ -65,6 +77,38 @@ func runDebugImport(env *command.Env, dbPath, jsonPath string) error {
 		return err
 	}
 	fmt.Fprintf(env, "Imported %q into %q\n", jsonPath, dp)
+	return nil
+}
+
+var hpFlags struct {
+	Length  int  `flag:"n,The length of the password to generate"`
+	NoDigit bool `flag:"no-digits,Omit digits from the generated password"`
+	Symbols bool `flag:"symbols,Include punctuation in the generated password"`
+	Confirm bool `flag:"c,Confirm passphrase"`
+}
+
+// runDebugHashpass implements the "debug hashpass" subcommand.
+func runDebugHashpass(env *command.Env, input string) error {
+	if hpFlags.Length <= 0 {
+		return env.Usagef("the length (-n) must be positive")
+	}
+
+	salt, seed, ok := strings.Cut(input, "@")
+	if !ok {
+		seed = input
+	}
+	pp, err := value.Cond(hpFlags.Confirm, kflib.ConfirmPassphrase, kflib.GetPassphrase)("Passphrase: ")
+	if err != nil {
+		return err
+	}
+	cs := kflib.Letters
+	if !hpFlags.NoDigit {
+		cs |= kflib.Digits
+	}
+	if hpFlags.Symbols {
+		cs |= kflib.Symbols
+	}
+	fmt.Println(kflib.HashedChars(hpFlags.Length, cs, pp, seed, salt))
 	return nil
 }
 
