e4fe0fb564b913126da50f420d0f60f6f67fd5de
M. J. Fromberger <michael.j.fromberger@gmail.com>
2026-09-02 20:41:31 -0700

    cmd/kf: add db export subcommand
    This allows export of records to the CSV format understood by the Apple
    "Passwords" application.

--- cmd/kf/internal/cmddb/cmddb.go
+++ cmd/kf/internal/cmddb/cmddb.go
@@ -2,14 +2,19 @@
 package cmddb
 
 import (
+	"cmp"
+	"encoding/csv"
 	"errors"
 	"fmt"
 	"os"
+	"slices"
 
 	"github.com/creachadair/command"
+	"github.com/creachadair/flax"
 	"github.com/creachadair/keyfish/cmd/kf/config"
 	"github.com/creachadair/keyfish/kfdb"
 	"github.com/creachadair/keyfish/kflib"
+	"github.com/creachadair/mds/slice"
 )
 
 var Command = &command.C{
@@ -32,6 +37,25 @@ var Command = &command.C{
 			Name: "edit",
 			Help: "Edit the full content of the database.",
 			Run:  command.Adapt(runDBEdit),
+		},
+		{
+			Name:  "export",
+			Usage: "[query...]",
+			Help: `Export the database to CSV.
+
+With no arguments, all non-archived records are exported.  Otherwise, only
+records matching the specified queries are chosen. Use "-a" to include archived
+records; use "-n" to exclude non-archived records.
+
+An --output file path is required. Caution: The output includes plaintext
+passwords and OTP secrets. You may specify "-" as the output to write to
+stdout, but this is not recommended unless you are piping to another program.
+
+By default, notes are omitted; use --notes to include them.
+By default, OTP codes are included; use --no-otp to exclude them.
+`,
+			SetFlags: command.Flags(flax.MustBind, &exportFlags),
+			Run:      command.Adapt(runDBExport),
 		},
 	},
 }
@@ -95,5 +119,77 @@ func runDBEdit(env *command.Env) error {
 		return err
 	}
 	fmt.Fprintf(env, "Edit applied to %q\n", config.DBPath(env))
+	return nil
+}
+
+var exportFlags struct {
+	Target string `flag:"to,Output file path (use '-' for stdout)"`
+	Arch   bool   `flag:"a,Include archived entries in the export"`
+	NArch  bool   `flag:"n,Exclude unarchived entries from the export"`
+	Notes  bool   `flag:"notes,Include notes field in output"`
+	NoOTP  bool   `flag:"no-otp,Exclude OTP settings from output"`
+}
+
+// runDBExport implements the "db export" subcommand.
+func runDBExport(env *command.Env, queries ...string) error {
+	if exportFlags.Target == "" {
+		return env.Usagef("a --to file is required (use '-' for stdout)")
+	}
+	s, err := config.LoadDB(env)
+	if err != nil {
+		return err
+	}
+	wantArchived := exportFlags.Arch || exportFlags.NArch
+	toExport := slice.Select(s.DB().Records, func(r *kfdb.Record) bool {
+		return r.Archived && wantArchived || !r.Archived && !exportFlags.NArch
+	})
+	if len(queries) != 0 {
+		var hits []*kfdb.Record
+		for _, q := range queries {
+			res, err := kflib.FindRecord(s.DB(), q, wantArchived)
+			if err != nil {
+				return err
+			}
+			hits = append(hits, res.Record)
+		}
+		toExport = slices.Values(hits)
+	}
+	f := os.Stdout
+	if exportFlags.Target != "-" { // already checked for empty above
+		f, err = os.Create(exportFlags.Target)
+		if err != nil {
+			return fmt.Errorf("create output file: %w", err)
+		}
+		defer f.Close() // in case of error
+	}
+
+	// TODO(creachadair): These are just the fields Apple wants.
+	cw := csv.NewWriter(f)
+	cw.Write([]string{"Title", "URL", "Username", "Password", "Notes", "OTPAuth"})
+	var nexp int
+	for r := range toExport {
+		nexp++
+		user := r.Username
+		if user == "" && len(r.Addrs) != 0 {
+			user = r.Addrs[0]
+		}
+		title := cmp.Or(r.Title, r.Label)
+		var url, notes, otpAuth string
+		if len(r.Hosts) != 0 {
+			url = r.Hosts[0]
+		}
+		if !exportFlags.NoOTP && r.OTP != nil {
+			otpAuth = r.OTP.String()
+		}
+		if exportFlags.Notes {
+			notes = r.Notes
+		}
+		cw.Write([]string{title, url, user, r.Password, notes, otpAuth})
+	}
+	cw.Flush()
+	if err := f.Close(); err != nil {
+		return err
+	}
+	fmt.Fprintf(env, "Exported %d records\n", nexp)
 	return nil
 }
